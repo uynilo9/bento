0a54599695b93900a3150f3c299c9747dac13aa9
uynilo9 <uynilo9@outlook.com>
2024-05-26 12:18:03 +0800

    Modify the bento.go file and add `--license, -l` flags

--- cmd/bento/bento.go
+++ cmd/bento/bento.go
@@ -22,10 +22,11 @@ type run struct {
 type args struct {
 	Run *run `arg:"subcommand:run" help:"run file by setting the argument <file>"`
 	Version bool `arg:"--version,-v" help:"display the version and exit"`
+	License bool `arg:"--license,-l" help:"display the license and exit"`
 }
 
 func (args) Description() string {
-	return "🍱 Welcome to Bento (c) " + os.Getenv("YEAR") + " @uynilo9\n"
+	return "🍱 Welcome to Bento " + os.Getenv("VERSION") + "\n"
 }
 
 func (args) Epilogue() string {
@@ -34,7 +35,7 @@ func (args) Epilogue() string {
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-		logger.Fatalf("Failed to find or load the file `.env` while running the command `%v`\n", cmd)
+		logger.Fatalf("Failed to find or load the file `.env` while running the command `%s`\n", cmd)
 	}
 	var args args
 	goarg, err := arg.NewParser(arg.Config{Program: os.Args[0], Exit: os.Exit}, &args)
@@ -42,33 +43,38 @@ func main() {
 		logger.Fatal("Failed to create the argument parser\n")
 	}
 	goarg.Parse(os.Args[1:])
-	if args.Version {
-		fmt.Println("🍱 Bento " + os.Getenv("VERSION") + " (c) " + os.Getenv("YEAR") + " @uynilo9")
-		os.Exit(0)
-	} else if args.Run != nil {
-		file := args.Run.File
-		if file != "" {
-			if path, err := filepath.Abs(file); err != nil {
-				logger.Fatalf("Failed to resolve the input file path `%v`\n", file)
-			} else {
-				if bytes, err := os.ReadFile(path); err != nil {
-					if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
-						logger.Fatalf("The input file `%v` doesn't exist\n", path)
-					} else {
-						logger.Fatalf("Failed to read the input file `%v`\n", path)
-					}
-				} else if filepath.Ext(file) != ".bento" {
-					logger.Fatalf("`%v` isn't a legal Bento source file\n", path)
+	switch {
+		case args.Version:
+			fmt.Println("🍱 Bento " + os.Getenv("VERSION"))
+			os.Exit(0)
+		case args.License:
+			fmt.Println("📜 Apache License 2.0 Copyright " + os.Getenv("YEAR") + " @uynilo9")
+			os.Exit(0)
+		case args.Run != nil:
+			file := args.Run.File
+			if file != "" {
+				if path, err := filepath.Abs(file); err != nil {
+					logger.Fatalf("Failed to resolve the input file path `%s`\n", file)
 				} else {
-					source := string(bytes)
-					fmt.Println(source)
+					if bytes, err := os.ReadFile(path); err != nil {
+						if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+							logger.Fatalf("The input file `%s` doesn't exist\n", path)
+						} else {
+							logger.Fatalf("Failed to read the input file `%s`\n", path)
+						}
+					} else if filepath.Ext(file) != ".bento" {
+						logger.Fatalf("`%s` isn't a legal Bento source file\n", path)
+					} else {
+						source := string(bytes)
+						fmt.Println(source)
+						os.Exit(0)
+					}
 				}
+			} else {
+				logger.Error("The argument <file> was missing while running the `run` subcommand\n")
+				goarg.WriteHelp(os.Stdout)
 			}
-		} else {
-			logger.Error("The argument <file> was missing while running the `run` subcommand\n")
+		default:
 			goarg.WriteHelp(os.Stdout)
-		}
-	} else {
-		goarg.WriteHelp(os.Stdout)
 	}
 }
